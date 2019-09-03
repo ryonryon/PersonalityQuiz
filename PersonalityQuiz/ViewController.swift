1b0d68c1fe98e45737300093e3170abb86e9973d
ryotogashi <ryo.togashi.ca@gmail.com>
2019-09-03 10:46:20 -0300

    create project

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Ryo Togashi on 2019-09-03.
+//  Copyright © 2019 Ryo Togashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
